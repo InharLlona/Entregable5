a6298eb786bbbcb7765875c3836f9063011b6c50
InharLlona <inharllona@gmail.com>
2021-12-14 16:59:33 +0100

    ok

---
+++ js.js
@@ -0,0 +1,43 @@
+const carrito = [
+    {
+    id: 198752,
+    name: "Tinta DJ27 Color",
+    price: 52.95,
+    count: 3,
+    premium: true
+    },
+    {
+    id: 75621,
+    name: "Impresora ticketera PRO-201",
+    price: 32.75,
+    count: 2,
+    premium: true
+    },
+    {
+    id: 54657,
+    name: "Caja de rollos de papel para ticketera",
+    price: 5.95,
+    count: 3,
+    premium: false
+    },
+    {
+    id: 3143,
+    name: "Caja de folios DIN-A4 80gr",
+    price: 9.95,
+    count: 2,
+    premium: false
+    }
+   ];
+//////Listado del carrito
+   var flag = 0;
+   for(product in carrito){
+       if(carrito[produc].premium == false){
+           flag=1;
+       }
+       if(product==carrito.length-1 && flag==1){
+        console.log("Este pedido tiene gastos de envío");
+       }else{
+        console.log("Pedido sin gastos de envío");
+       }
+
+   }
