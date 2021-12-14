c894d9b218a28f0f1f471b5124596e678341a116
InharLlona <inharllona@gmail.com>
2021-12-14 19:08:11 +0100

    ok

--- js.js
+++ js.js
@@ -3,31 +3,122 @@ const carrito = [
     id: 198752,
     name: "Tinta DJ27 Color",
     price: 52.95,
-    count: 3,
+    count: 0,
     premium: true
     },
     {
     id: 75621,
     name: "Impresora ticketera PRO-201",
     price: 32.75,
-    count: 2,
+    count: 0,
     premium: true
     },
     {
     id: 54657,
     name: "Caja de rollos de papel para ticketera",
     price: 5.95,
-    count: 3,
+    count: 0,
     premium: true
     },
     {
     id: 3143,
     name: "Caja de folios DIN-A4 80gr",
     price: 9.95,
-    count: 2,
+    count: 0,
     premium: false
     }
    ];
+   escribirnombres();
+   escribirid();
+   escribirprecio();
+   escribirpreciototal();
+   esprime();
+
+   document.getElementById("idCantidad1").addEventListener("change",()=>cambiar())
+   document.getElementById("idCantidad2").addEventListener("change",()=>cambiar())
+   document.getElementById("idCantidad3").addEventListener("change",()=>cambiar())
+   document.getElementById("idCantidad4").addEventListener("change",()=>cambiar())
+   document.getElementById("idPrime1").addEventListener("change",()=>esprime())
+   document.getElementById("idPrime2").addEventListener("change",()=>esprime())
+   document.getElementById("idPrime3").addEventListener("change",()=>esprime())
+   document.getElementById("idPrime4").addEventListener("change",()=>esprime())
+   
+var cambiar = () =>{
+    for(unidades in carrito){
+        var strr = concatstring("idCantidad",parseFloat(unidades)+1);
+       carrito[unidades].count = document.getElementById(strr).value;
+     }
+     escribirpreciototal();
+}
+
+function escribirnombres () {
+    for(unidades in carrito){
+       var strr = concatstring("idNombre",parseFloat(unidades)+1);
+       document.getElementById(strr).innerHTML = carrito[unidades].name;
+    }
+}
+function escribirprecio () {
+    for(unidades in carrito){
+       var strr = concatstring("idPrecio",parseFloat(unidades)+1);
+       document.getElementById(strr).innerHTML = carrito[unidades].price + "€/u";
+    }
+}
+function escribirid () {
+    for(unidades in carrito){
+       var strr = concatstring("id",parseFloat(unidades)+1);
+       document.getElementById(strr).innerHTML = carrito[unidades].id;
+    }
+}
+function escribirpreciototal () {
+    var suma = 0;
+    for(unidades in carrito){
+       var strr = concatstring("idPreciot",parseFloat(unidades)+1);
+       mult=carrito[unidades].price * carrito[unidades].count;
+       document.getElementById(strr).innerHTML ="Precio total del producto es de " + mult.toFixed(2) +" €";
+       suma = suma + carrito[unidades].price * carrito[unidades].count;
+       }
+    if(suma>100){
+    document.getElementById("idPreciototal").innerHTML = "El precio total de la compra es de " +(carrito[unidades].price * carrito[unidades].count).toFixed(2) +" €, pero al ser su compra de un importe superior a 100€ le descontaremos el 5%. Por lo que el precio final es de "+ (suma * 0.95).toFixed(2) + " €" ;
+    }else{
+        document.getElementById("idPreciototal").innerHTML = "El precio total de la compra es de " +suma.toFixed(2) +" €" ;
+    } 
+}
+function esprime () {
+    for(unidades in carrito){
+       var strr = concatstring("idPrime",parseFloat(unidades)+1);
+       document.getElementById(strr).checked = carrito[unidades].premium;
+    }
+}
+
+
+function concatstring (str,i){
+return str + i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////Listado del carrito
    var flag = 0;
    for(product in carrito){
