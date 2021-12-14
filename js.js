cb567078a014b473c6138425dda78731adac2251
InharLlona <inharllona@gmail.com>
2021-12-14 16:51:39 +0100

    ok

--- js.js
+++ js.js
@@ -32,34 +32,5 @@ const carrito = [
    for(product in carrito){
        console.log(product,"+",carrito[product], "color=blue");
    }
-//////Elimino el elemento con el id 54657
-    for(cosas in carrito){
-       //console.log(cosas,carrito[cosas].id);
-       if(carrito[cosas].id == "54657"){
-        console.log( "apache" + cosas);
-           var jcarrito = carrito.splice(cosas,1);
-       }
-      if (cosas == carrito.length-1){
-        for(jproduct in carrito){
-            console.log(jproduct,"+",carrito[jproduct]);
-        }
-      }
-    }
-//////
-    var suma = 0;
-    for(unidades in carrito){
-        var suma;
-         suma= suma + carrito[unidades].price * carrito[unidades].count;
-         if (unidades == carrito.length-1){
-                console.log("Total"+ suma + "€");
-          }
-    }
-//////
-    var suma = 0;
-        for(unidades in carrito){
-            if (carrito[unidades].premium == true){
-                console.log(carrito[unidades]);
-            }
-    }
-    
+ 
     
