4e598be29baa5d185686790c30f7f68fb53b193e
InharLlona <inharllona@gmail.com>
2021-12-14 16:48:34 +0100

    ok

--- js.js
+++ js.js
@@ -28,33 +28,8 @@ const carrito = [
     premium: false
     }
    ];
-//////Listado del carrito
-   for(product in carrito){
-       console.log(product,"+",carrito[product], "color=blue");
-   }
-//////Elimino el elemento con el id 54657
-    for(cosas in carrito){
-       //console.log(cosas,carrito[cosas].id);
-       if(carrito[cosas].id == "54657"){
-        console.log( "apache" + cosas);
-           var jcarrito = carrito.splice(cosas,1);
-       }
-      if (cosas == carrito.length-1){
-        for(jproduct in carrito){
-            console.log(jproduct,"+",carrito[jproduct]);
-        }
-      }
-    }
-//////
-    var suma = 0;
-    for(unidades in carrito){
-        var suma;
-         suma= suma + carrito[unidades].price * carrito[unidades].count;
-         if (unidades == carrito.length-1){
-                console.log("Total"+ suma + "€");
-          }
-    }
-//////
+
+//////Mostrar elementos que sean prime
     var suma = 0;
         for(unidades in carrito){
             if (carrito[unidades].premium == true){
